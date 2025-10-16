2b5c520cdfa91291f143dd254e86d9d24bbfbdcc
MarianaSiano <marianasianop@gmail.com>
2025-10-16 16:28:05 -0300

    Adicionando os tempos e as posições

--- Atividade_3-3.py
+++ Atividade_3-3.py
@@ -11,3 +11,21 @@ def calcular_vetor_posicao(t):
     Returns:
     numpy.ndarray: Um array NumPy representando o vetor posição [x, y] em centímetros (cm).
     """
+
+    #Componente x do vetor
+    x = 4.0 + (2.5 * (t ** 2))
+
+    #Componente y do vetor
+    y = 5.0 * t
+
+    #Retorna o vetor como um array NumPy [x, y]
+    return np.array([x, y])
+
+# Atividade
+#Definir o tempo para o qual queremos calcular
+t1 = 0.0 #em segundos
+t2 = 2.0 #em segundos
+
+#Chamar a função para obter o vetor posição
+p1 = calcular_vetor_posicao(t1)
+p2 = calcular_vetor_posicao(t2)
