05641f8cc040c1de22cf2c4d514d4f7e364a35f8
MarianaSiano <marianasianop@gmail.com>
2025-10-21 11:21:21 -0300

    Letra b concluida

--- atividade_3-10.py
+++ atividade_3-10.py
@@ -8,3 +8,7 @@ v_x = 60 # Velocidade horizontal (m/s)
 # a) Tempo para atingir o solo
 t_total = np.sqrt((2 * h) / g)
 print(f"Tempo para atingir o solo: {t_total: .2f} s")
+
+# b) Distância horizontal
+x_total = v_x * t_total
+print(f"Distância horizontal: {x_total: .2f} m")
