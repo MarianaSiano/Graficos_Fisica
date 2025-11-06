7924bc90f857a2b1dc1a3ec3b7b4e8da9ef01d6d
MarianaSiano <marianasianop@gmail.com>
2025-11-06 17:43:36 -0300

    Atualizando a Normal

--- Atividade_3-51.py
+++ Atividade_3-51.py
@@ -3,7 +3,7 @@ import matplotlib.patches as patches
 
 # Valores aproximados para as forças (ajuste F_normal conforme cálculo)
 peso = 75 * 9.8 # Peso em N
-F_normal = 2500 # Exemplo: valor a ser ajustado conforme o cálculo correto
+F_normal = 3800
 
 fig, ax = plt.subplots(figsize=(4, 7))
 
